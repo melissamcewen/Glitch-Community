3859d142dc28267bb7101f2a66199f64f509b898
Glitch (community-staging) <none>
2019-06-18 19:10:24 +0000

    📒👏 Checkpoint ./src/components/text/text.js:301412/1626 ./src/components/text/text.styl:301412/186

--- src/components/text/text.js
+++ src/components/text/text.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
 import styles from './text.styl';
+
+const cx = classNames.bind(styles);
 
 /**
  * Text Component
  */
-const Text = ({ children, defaultMargin }) => <p className={styles.p}>{children}</p>;
+const Text = ({ children, defaultMargin }) => {
+  const className = cx({
+    p: true,
+    defaultMargin,
+  });
+  
+  return <p className={className}>{children}</p>;
+};
 
 Text.propTypes = {
   /** element(s) to display in the tag */
   children: PropTypes.node.isRequired,
+  /** use the browser-defined paragraph margins? */
+  defaultMargin: PropTypes.bool,
 };
+
+Text.defaultProps = {
+  defaultMargin: false,
+}
 
 export default Text;
