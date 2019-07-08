2ec57b230b8c6058c1bd745c3e36a5f6130569a4
Glitch (community-staging) <none>
2019-07-08 02:20:00 +0000

    💕👩 Checkpoint ./src/presenters/pages/create.js:708175/61 ./src/components/text/heading.js:708175/142

--- src/presenters/pages/create.js
+++ src/presenters/pages/create.js
@@ -53,8 +53,8 @@ function Banner() {
     <section className={classNames(styles.section, styles.banner)}>
       <div className={styles.bannerShape} style={{ backgroundImage: `url(${shape})` }}>
         <div className={styles.bannerText}>
-          <Heading className={styles.bannerTagline} tagName="h1">
-            <Mark color="#fbf2b8">Create </Mark>
+          <Heading className={styles.bannerTagline} tagName="h1" ariaLabel='Create the app of your dreams'>
+            <Mark color="#fbf2b8">Create</Mark>
             <Unmarked>the app of your dreams</Unmarked>
           </Heading>
           <Text>Whether you're new to code or an experienced developer, Glitch is the fastest tool for turning your ideas into web apps.</Text>
