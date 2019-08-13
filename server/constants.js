b115efcc90d8d79f3416726408c8e3a9e9849ef8
Glitch (community-staging) <none>
2019-08-13 21:00:09 +0000

    📇😀 Checkpoint ./server/constants.js:355867/10

--- server/constants.js
+++ server/constants.js
@@ -2,7 +2,7 @@ const { envs, tagline } = require('../shared/constants');
 
 // in the backend, just switch between staging and production
 // the client supports RUNNING_ON = development
-const currentEnv = process.env.RUNNING_ON === 'staging' ? 'staging' : 'production';
+const currentEnv = 'local';//process.env.RUNNING_ON === 'staging' ? 'staging' : 'production';
 module.exports = {
   ...envs,
   current: envs[currentEnv],
