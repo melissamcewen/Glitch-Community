c4cde17f5a611dfd9086f9f95756bbed393c4f86
Glitch (community-staging) <none>
2019-08-13 21:44:12 +0000

    🎈👙 Checkpoint ./webpack.config.js:1978864/225

--- webpack.config.js
+++ webpack.config.js
@@ -79,21 +79,21 @@ module.exports = smp.wrap({
   },
   module: {
     rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        include: SRC,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          fix: false, //mode === 'development', // Only change source files in development
-          cache: false, // Keep this off, it can use a lot of space.  Let Webpack --watch do the heavy lifting for us.
-          emitError: false,
-          emitWarning: true,
-          failOnError: false,
-          ignorePattern: 'src/curated/**',
-        },
-      },
+      // {
+      //   enforce: 'pre',
+      //   test: /\.js$/,
+      //   include: SRC,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   options: {
+      //     fix: false, //mode === 'development', // Only change source files in development
+      //     cache: false, // Keep this off, it can use a lot of space.  Let Webpack --watch do the heavy lifting for us.
+      //     emitError: false,
+      //     emitWarning: true,
+      //     failOnError: false,
+      //     ignorePattern: 'src/curated/**',
+      //   },
+      // },
       {
         oneOf: [
           {
