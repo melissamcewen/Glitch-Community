8c8dad75dc20df6073a636b87bb2b2a31135a4cf
Glitch (community-staging) <none>
2019-08-16 18:36:05 +0000

    🎣📚 Checkpoint ./webpack.config.js:301412/13

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = smp.wrap({
     path: BUILD,
     publicPath: '/',
   },
-  devtool: mode === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+  devtool: mode === 'production' ? 'source-map' : 'eval-source-map',
   // devtool: 'source-map',
   optimization: {
     splitChunks: {
