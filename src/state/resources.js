22d4554f998ef219868019b786bdd07727b8e100
Glitch (community-staging) <none>
2019-09-03 18:07:02 +0000

    🚧🏆 Checkpoint ./src/state/resources.js:271885/11356

--- src/state/resources.js
+++ src/state/resources.js
@@ -4,7 +4,7 @@ const DEFAULT_TTL = 1000 * 60 * 5; // 5 minutes
 const status = {
   loading: 'loading',
   ready: 'ready',
-}
+};
 
 const resourceConfig = {
   collections: {
@@ -54,7 +54,17 @@ state shape:
 
 const initialState = mapValues(resourceConfig, () => ({}));
 
-const hasExpired = (row) => 
+const rowIsMissingOrExpired = (row) => {
+  if (!row) return true;
+  if (row.status === status.loading) return false;
+  return row.expires < Date.now();
+};
+
+const getChildResourceType = (type, childType) => {
+  const childResourceType = resourceConfig[type].refernces[childType];
+  if (!childResourceType) throw new Error(`Unknown reference type "${childType}"`);
+  return childResourceType;
+};
 
 /*
 get the cached resource + any requests that we need to make
@@ -70,19 +80,18 @@ const getResource = (state, type, id) => {
 
   const row = state[type][id];
   // resource is missing or expired; request the resource
-  if (!row || row.expires < Date.now()) {
+  if (rowIsMissingOrExpired(row)) {
     return { status: status.loading, value: null, requests: [{ type, ids: [id] }] };
   }
-  
+
   return { status: row.status, value: row.value, requests: [] };
 };
 
 const getChildResources = (state, type, id, childType) => {
   if (!resourceConfig[type]) throw new Error(`Unknown resource type "${type}"`);
-  const childResourceType = resourceConfig[type].refernces[childType];
-  if (!childResourceType) throw new Error(`Unknown reference type "${childType}"`);
-  
-  const parentRow = state[type][id]
+  const childResourceType = getChildResourceType(type, childType);
+
+  const parentRow = state[type][id];
   // resource isn't present or list of child IDs are expired; request the children
   if (!parentRow) {
     return { status: status.loading, value: null, requests: [{ type, id, childType }] };
@@ -90,12 +99,12 @@ const getChildResources = (state, type, id, childType) => {
 
   const childIDsRequest = parentRow.references[childType];
   // resource is present but its children are missing or expired; request all of its children
-  if (!childIDsRequest || childIDsRequest.expires < Date.now()) {
+  if (rowIsMissingOrExpired(childIDsRequest)) {
     return { status: status.loading, value: null, requests: [{ type, id, childType }] };
   }
   // child IDs request is pending; no request needed but no results yet
   if (childIDsRequest.status === status.loading) {
-    return { status: status.loading, value: null, requests: [] }
+    return { status: status.loading, value: null, requests: [] };
   }
 
   // collect all of the associated children from the child resource table
@@ -120,45 +129,44 @@ const getChildResources = (state, type, id, childType) => {
 const getOrInitializeRow = (state, type, id) => {
   // create row with reference map if it doesn't exist already
   if (!state[type][id]) {
-    state[type][id] = { 
-      references: mapValues(resourceConfig[type].references, () => ({})) 
-    }
+    state[type][id] = {
+      references: mapValues(resourceConfig[type].references, () => ({})),
+    };
   }
-  return state[type][id]
-}
-
+  return state[type][id];
+};
 
 const storePendingRequests = (state, { type, ids }) => {
   for (const id of ids) {
-    const row = getOrInitializeRow(state, type, id)
-    row.status = status.loading
-    row.expires = null
-  }  
-}
+    const row = getOrInitializeRow(state, type, id);
+    row.status = status.loading;
+    row.expires = null;
+    row.value = null;
+  }
+};
 
 const storePendingChildRequests = (state, { type, id, childType }) => {
-  
-}
+  const row = getOrInitializeRow(state, type, id);
+  row.references[childType] = { status:  }
+};
 
 // { type, values: [{ id, ...fields }] }
 const storeResources = (state, { type, values }) => {
-  const expires = Date.now() + DEFAULT_TTL
+  const expires = Date.now() + DEFAULT_TTL;
   for (const value of values) {
-    const row = getOrInitializeRow(state, type, value.id)
-    row.status = status.ready
-    row.expires = expires
-    row.value = value
+    const row = getOrInitializeRow(state, type, value.id);
+    row.status = status.ready;
+    row.expires = expires;
+    row.value = value;
   }
-}
+};
 
 const storeChildResources = (state, { type, id, childType, values }) => {
-  const childResourceType = resourceConfig[type].references[childType];
-  if (!childResourceType) throw new Error(`Unknown reference type "${childType}"`);
-  
+  const childResourceType = getChildResourceType(type, childType);
   // store IDs on parent
-  const row = getOrInitializeRow(state, type, id)
-  row.references[childType] = { status: status.ready, ids: values.map(value => value.id) }
-  
+  const row = getOrInitializeRow(state, type, id);
+  row.references[childType] = { status: status.ready, ids: values.map((value) => value.id) };
+
   // store children
-  storeResources(state, { type: childResourceType, values })
-}
+  storeResources(state, { type: childResourceType, values });
+};
